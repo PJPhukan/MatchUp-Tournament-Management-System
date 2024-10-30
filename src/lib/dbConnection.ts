dcb0fee36d8fa4f8db7ab1a26aebf11b90a8768a
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-10-30 23:04:45 +0530

    "Update title in metadata, add success console statement"

--- src/lib/dbConnection.ts
+++ src/lib/dbConnection.ts
@@ -5,14 +5,14 @@ import { ConnectionObject } from "@/types/dbConnectionObject"
 const connection: ConnectionObject = {};
 
 
-const dbConnect = async (): Promise<void> => {
+export const dbConnect = async (): Promise<void> => {
     if (connection.isConnected) {
         return;
     }
     try {
         const db = await mongoose.connect(process.env.MONGO_DB_URI || " ")
         connection.isConnected = db.connections[0].readyState;
-
+        console.log("Successfully database connected");
     } catch (error) {
         console.log("Database connection error")
         process.exit(1);
