f901dbec25f41daae452146c501cb62e50f88d2f
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-11-03 19:25:52 +0530

    feat : " Added create, delete, update and get details of a tournament route"

---
+++ src/app/api/(tounament)/[tournamentId]/route.ts
@@ -0,0 +1,271 @@
+import { ApiResponse } from "@/helpers/ApiResponse";
+import { decodeToken } from "@/helpers/authHelpers";
+import { dbConnect } from "@/lib/dbConnection";
+import { TournamentModel } from "@/model/tournament.model";
+import { UserModel } from "@/model/user.model";
+import { Tournament } from "@/types/tournamentTypes";
+import { NextRequest, NextResponse } from "next/server";
+
+//delete a tournament
+export async function DELETE(request: NextRequest, { params }: { params: { tournamentId: string } }): Promise<NextResponse> {
+
+    await dbConnect();
+
+    try {
+        // Access tournamentId from params
+        const { tournamentId } = params;
+        //decode payload from the request
+        const tokenId = decodeToken(request);
+
+        // Check if the user is authenticated and verified
+        if (!tokenId) {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid or expired token.",
+                statusCode: 401
+            });
+        }
+
+
+        //check tournament exist or not by tournament id
+        let tournament = await TournamentModel.findById(tournamentId)
+
+        //if tournament not exist then return with a error message
+        if (!tournament) {
+            return NextResponse.json({
+                success: false,
+                message: "Tournament not found.",
+                statusCode: 404
+            });
+        }
+
+        //check user exist or not by user id
+        let user = await UserModel.findById(tokenId)?.select("-password")
+
+        //check user is authenticated or not
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: "User not found.",
+                statusCode: 404
+            });
+        }
+
+        //check user is verified or not
+        if (!user.isVerified) {
+            return NextResponse.json({
+                success: false,
+                message: "User is not verified.",
+                statusCode: 401
+            });
+        }
+
+        //delete tournament TODO:  Also delete all games under this tournament
+        const deleteTournament = await TournamentModel.findByIdAndDelete(tournamentId);
+
+        //check success or not
+        if (!deleteTournament) {
+            return NextResponse.json({
+                success: false,
+                message: "Failed to delete tournament.",
+                statusCode: 401
+            });
+        }
+
+        //send successfull message to the user
+        let response = {
+            success: true,
+            message: "Tournament deleted successfully",
+            statusCode: 201,
+        }
+        return NextResponse.json(response);
+
+
+    } catch (error) {
+        console.error("Error while deleting tournament", error);
+        const response: ApiResponse = {
+            success: false,
+            message: "Failed to delete tournament",
+            statusCode: 401
+        }
+        return NextResponse.json(response);
+
+    }
+}
+
+
+//get tournament details
+export async function GET(request: NextRequest, { params }: { params: { tournamentId: string } }): Promise<NextResponse> {
+
+    await dbConnect();
+
+    try {
+        // Access tournamentId from params
+        const { tournamentId } = params;
+
+        //decode payload from the request
+        const tokenId = decodeToken(request);
+
+        // Check if the user is authenticated and verified
+        if (!tokenId) {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid or expired token.",
+                statusCode: 401
+            });
+        }
+
+
+        //check tournament exist or not by tournament id
+        let tournament = await TournamentModel.findById(tournamentId)
+
+        //if tournament not exist then return with a error message
+        if (!tournament) {
+            return NextResponse.json({
+                success: false,
+                message: "Tournament not found.",
+                statusCode: 404
+            });
+        }
+
+        //check user exist or not by user id
+        let user = await UserModel.findById(tokenId)?.select("-password")
+
+        //check user is authenticated or not
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: "User not found.",
+                statusCode: 404
+            });
+        }
+
+        //check user is verified or not
+        if (!user.isVerified) {
+            return NextResponse.json({
+                success: false,
+                message: "User is not verified.",
+                statusCode: 401
+            });
+        }
+
+        //send successfull message to the user
+        let response = {
+            success: true,
+            message: "Tournament details fatch successfully successfully",
+            statusCode: 201,
+            data: tournament
+        }
+        return NextResponse.json(response);
+
+
+    } catch (error) {
+        console.error("Error while deleting tournament", error);
+        const response: ApiResponse = {
+            success: false,
+            message: "Failed to fetch tournament details",
+            statusCode: 401
+        }
+        return NextResponse.json(response);
+
+    }
+}
+
+
+
+//update tournament details
+export async function PUT(request: NextRequest, { params }: { params: { tournamentId: string } }): Promise<NextResponse> {
+    await dbConnect();
+
+    try {
+        // Access tournamentId from params
+        const { tournamentId } = params;
+
+        // Extract JSON data from the request body
+        const { name, startDate, endDate, location, description } = await request.json();
+
+
+        //decode payload from the request
+        const tokenId = decodeToken(request);
+
+        // Check if the user is authenticated and verified
+        if (!tokenId) {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid or expired token.",
+                statusCode: 401
+            });
+        }
+
+        //check tournament exist or not by tournament id
+        let tournament = await TournamentModel.findById(tournamentId)
+
+        //if tournament not exist then return with a error message
+        if (!tournament) {
+            return NextResponse.json({
+                success: false,
+                message: "Tournament not found.",
+                statusCode: 404
+            });
+        }
+
+        //check user exist or not by user id
+        let user = await UserModel.findById(tokenId)?.select("-password")
+
+        //check user is authenticated or not
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: "User not found.",
+                statusCode: 404
+            });
+        }
+
+        //check user is verified or not
+        if (!user.isVerified) {
+            return NextResponse.json({
+                success: false,
+                message: "User is not verified.",
+                statusCode: 401
+            });
+        }
+
+        //update tournament
+        tournament.name = name;
+        tournament.startDate = startDate;
+        tournament.endDate = endDate;
+        tournament.description = description;
+        tournament.location = location;
+
+        const updatedTournament = await tournament.save();
+
+        //check success or not
+        if (!updatedTournament) {
+            return NextResponse.json({
+                success: false,
+                message: "Failed to update tournament.",
+                statusCode: 401
+            });
+        }
+
+        //send successfull message to the user
+        let response = {
+            success: true,
+            message: "Tournament updated successfully",
+            statusCode: 201,
+            data: updatedTournament
+        }
+        return NextResponse.json(response);
+
+
+    } catch (error) {
+        console.error("Error while updating tournament", error);
+        const response: ApiResponse = {
+            success: false,
+            message: "Failed to update tournament",
+            statusCode: 401
+        }
+        return NextResponse.json(response);
+
+    }
+}
---
+++ src/app/api/(tounament)/c-tournament/route.ts
@@ -0,0 +1,176 @@
+
+import { ApiResponse } from "@/helpers/ApiResponse";
+import { decodeToken } from "@/helpers/authHelpers";
+import { dbConnect } from "@/lib/dbConnection";
+import { TournamentModel } from "@/model/tournament.model";
+import { UserModel } from "@/model/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+
+
+//create a new tournament
+export async function POST(request: NextRequest): Promise<NextResponse> {
+
+    await dbConnect();
+
+    try {
+
+        // Extract JSON data from the request body
+        const { name, poster, startDate, endDate, location, description } = await request.json();
+
+
+        //decode payload from the request
+        const tokenId = decodeToken(request);
+
+        // Check if the user is authenticated and verified
+        if (!tokenId) {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid or expired token.",
+                statusCode: 401
+            });
+        }
+
+        //check user exist or not by user id
+        let user = await UserModel.findById(tokenId)?.select("-password")
+
+        //check user is authenticated or not
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: "User not found.",
+                statusCode: 404
+            });
+        }
+
+        //check user is verified or not
+        if (!user.isVerified) {
+            return NextResponse.json({
+                success: false,
+                message: "User is not verified.",
+                statusCode: 401
+            });
+        }
+        //TODO: Upload image 3rd party resources 
+        const posterUrl = ""
+
+        // create a new tournament
+        let tournament = await TournamentModel.create({
+            name,
+            poster: posterUrl,
+            startDate,
+            endDate,
+            location,
+            description,
+            userId: user._id
+        });
+
+        //check tournament is successfully created or not
+        if (!tournament) {
+            return NextResponse.json({
+                success: false,
+                message: "Failed to create a new tournament",
+                statusCode: 401
+            });
+        }
+
+        //send successfull message to the user
+        let response = {
+            success: true,
+            message: "Tournament created successfully",
+            statusCode: 201,
+            data: tournament
+        }
+        return NextResponse.json(response);
+
+
+    } catch (error) {
+        console.error("Error while creating a new tournament", error);
+        const response: ApiResponse = {
+            success: false,
+            message: "Failed to create a new tournament",
+            statusCode: 401
+        }
+        return NextResponse.json(response);
+
+    }
+}
+
+
+//get all tournament which is created by the user
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+
+    await dbConnect();
+    try {
+        //decode payload from the request
+        const tokenId = decodeToken(request);
+
+        // Check if the user is authenticated and verified
+        if (!tokenId) {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid or expired token.",
+                statusCode: 401
+            });
+        }
+
+        //check user exist or not by user id
+        let user = await UserModel.findById(tokenId)?.select("-password")
+
+        //check user is authenticated or not
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: "User not found.",
+                statusCode: 404
+            });
+        }
+
+        //check user is verified or not
+        if (!user.isVerified) {
+            return NextResponse.json({
+                success: false,
+                message: "User is not verified.",
+                statusCode: 401
+            });
+        }
+
+
+        //retrive all user created tournament
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //send successfull message to the user
+        let response = {
+            success: true,
+            message: "Tournament created successfully",
+            statusCode: 201
+        }
+        return NextResponse.json(response);
+
+
+    } catch (error) {
+        const response: ApiResponse = {
+            success: false,
+            message: "Failed to retrive tournaments",
+            statusCode: 401
+        }
+
+        return NextResponse.json(response);
+    }
+}
+
